a159eaa901f1574b5339a4a33bed5c1cda23dd6f
Andrea Soares <asoaresdev@gmail.com>
2023-08-04 15:17:07 -0300

    feat: endpoints get e post

--- src/database/poo.sql
+++ src/database/poo.sql
@@ -1,7 +1,13 @@
--- Active: 1691098610947@@127.0.0.1@1433
+-- Active: 1691104084090@@127.0.0.1@3306
 CREATE TABLE video(
     id TEXT  UNIQUE PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     dur_seconds REAL NOT NULL,
     date_upload TEXT DEFAULT (DATETIME())NOT NULL
 );
+
+INSERT INTO video(id, title, dur_seconds, date_upload)
+VALUES("001", "TESETE", 200, "2023/08/03");
+
+INSERT INTO video(id, title, dur_seconds, date_upload)
+VALUES("002", "TESTE2", 200, "2023/08/03")
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
+import { db } from "./database/knex"
+import { Video } from "./models/Video"
 
 const app = express()
 
@@ -12,4 +14,75 @@ app.listen(3003, () => {
 
 app.get("/ping", (req: Request, res: Response) => {
     res.status(200).send("PONG")
+})
+
+app.get("/videos", async (req: Request, res: Response) => {
+    try {
+        const result = await db("video")
+        const videos: Video[] = result.map((videoDB) =>
+            new Video(
+                videoDB.id,
+                videoDB.title,
+                videoDB.durSeconds,
+                videoDB.dateUpload
+            )
+        )
+        res.status(200).send(videos)
+
+    } catch (error) {
+        console.log(error);
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.post("/videos", async (req: Request, res: Response) => {
+    try {
+        const { id, title, durSeconds } = req.body
+        if (typeof id !== "string") {
+            res.status(400)
+            throw new Error("'id' deve ser string")
+        }
+        if (typeof title !== "string") {
+            res.status(400)
+            throw new Error("'title' deve ser string")
+        }
+        if (typeof durSeconds !== "number") {
+            res.status(400)
+            throw new Error("'durSeconds' deve ser number")
+        }
+
+        const [validateId] = await db("video").where({ id: id })
+        console.log(validateId);
+
+        if(validateId){
+            res.statusCode = 400
+            throw new Error("ID já cadastrado")
+        }
+        const newVideo = new Video(
+            id,
+            title, 
+            durSeconds,
+            new Date().toISOString()
+        )
+        console.log(newVideo);
+        
+        res.status(201).send(validateId)
+    } catch (error) {
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+        if(error instanceof Error){
+            res.send(error.message)
+        }else{
+            res.send("Erro inesperado")
+        }
+    }
 })
