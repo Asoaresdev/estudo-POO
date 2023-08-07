abb0c8142e768441e42060d542408390b3729dc1
Andrea Soares <asoaresdev@gmail.com>
2023-08-07 20:52:39 -0300

    refactor: conexão e get all videos em POO

---
+++ src/database/VideoDatabase.ts
@@ -0,0 +1,28 @@
+import { videoDB } from "../models/types"
+import { Video } from "../models/video"
+import { BaseDatabase } from "./BaseDatabase"
+
+export class VideoDatabase extends BaseDatabase {
+    public static TABLE_VIDEO = "video"
+
+    public async getAllVideos() {
+        const result: videoDB[] = await BaseDatabase
+            .connection(VideoDatabase.TABLE_VIDEO)  
+            
+        return result
+    }
+
+    public async findVideoById (id:string) {
+        const [videoDB]: Video[] = await BaseDatabase
+            .connection(VideoDatabase.TABLE_VIDEO)
+            .where({id:id})
+
+        return videoDB
+    }
+
+    public async insertVideo (newVideoDB: videoDB) {
+        await BaseDatabase
+            .connection(VideoDatabase.TABLE_VIDEO)
+            .insert(newVideoDB)
+    }
+}
